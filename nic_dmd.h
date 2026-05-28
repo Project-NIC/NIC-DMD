8162090779fb98385431e995204ccd5c3133099c
N.I.C. <n.i.c.laboratories@gmail.com>
2026-05-28 18:43:04 +0200

    Update nic_dmd.h

--- nic_dmd.h
+++ nic_dmd.h
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 #ifndef NIC_DMD_H
 #define NIC_DMD_H
 
@@ -16,7 +18,7 @@
 
 /* Konstanty protokolu */
 #define DMD_OUT_MAX (DMD_ENC_BUF_SIZE + 1)
-#define DMD_KEYFRAME_EVERY 8
+#define DMD_KEYFRAME_EVERY 7 // Hodnota 7 vyhrazena pro verzi protokolu
 
 typedef struct {
     uint8_t pkt_len;
@@ -33,6 +35,6 @@ void dmd_encoder_init(dmd_encoder_t *enc, uint8_t pkt_len);
 void dmd_decoder_init(dmd_decoder_t *dec, uint8_t pkt_len);
 
 uint8_t dmd_compress(dmd_encoder_t *enc, const uint8_t *current, uint8_t *output);
-void dmd_decompress(dmd_decoder_t *dec, const uint8_t *input, uint8_t in_len, uint8_t *output);
+int dmd_decompress(dmd_decoder_t *dec, const uint8_t *input, uint8_t in_len, uint8_t *output);
 
 #endif /* NIC_DMD_H */
